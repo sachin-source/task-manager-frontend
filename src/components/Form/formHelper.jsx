2b7cf92e914f9f3e88336d6de41f1d8b7b23acba
sachin-source <sachinmgvt@gmail.com>
2022-08-12 23:59:06 +0530

    task listing fix

--- src/components/Form/formHelper.jsx
+++ src/components/Form/formHelper.jsx
@@ -14,25 +14,25 @@ const useForm = (callback, validate) => {
         },
         body: JSON.stringify(values),
       })
-      .then((response) => response.json())
-      .then((data) => {
-        if(data?.auth){
-          localStorage.setItem("authToken", data?.authToken);
-          callback();
-        }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
+        .then((response) => response.json())
+        .then((data) => {
+          if (data?.auth) {
+            localStorage.setItem("authToken", data?.authToken);
+            callback();
+          }
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
     }
   }, [errors]);
-  
+
   const handleSubmit = event => {
     if (event) event.preventDefault();
     setErrors(validate(values));
     setIsSubmitting(true);
   };
-  
+
   const handleChange = event => {
     event.persist();
     setValues(values => ({
@@ -40,24 +40,23 @@ const useForm = (callback, validate) => {
       [event.target.name]: event.target.value
     }));
   };
-  
+
   const authenticateLogin = () => {
     const authToken = localStorage.getItem('authToken');
     fetch('http://localhost:3005/auth/', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-        'token' : authToken
+        'token': authToken
       },
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      localStorage.setItem('userData', JSON.stringify(data))
-      callback();
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
+    }).then((response) => response.json())
+      .then((data) => {
+        localStorage.setItem('userData', JSON.stringify(data))
+        callback();
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   }
 
   return {
--- src/components/Home/homeHelper.js
+++ src/components/Home/homeHelper.js
@@ -5,7 +5,22 @@ const useForm = (callback, validate) => {
   const [errors, setErrors] = useState({});
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const getTasks = () => {};
+  const getTasks = () => {
+    fetch('http://localhost:3005/task/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'token': authToken
+      },
+    }).then((response) => response.json())
+      .then((data) => {
+        console.log('task data')
+        callback();
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
   
   return { getTasks }
 }
