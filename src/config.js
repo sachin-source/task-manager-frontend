03d3fbdb9275b0c1400b2bb5fca90a3ca1159625
sachin-source <sachinmgvt@gmail.com>
2022-08-28 16:24:01 +0530

    footer visibility according to view port

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-const apiUrl =   "http://localhost:3000/" // "https://damp-garden-72926.herokuapp.com/"; //
+const apiUrl =    "http://localhost:3000/" // "https://damp-garden-72926.herokuapp.com/"; //
 module.exports = { apiUrl };
